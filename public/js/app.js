c3c66c60f290b06c15d986fdec05c11816156251
Hítalo Silva <hitalos@gmail.com>
2017-10-19 19:40:14 -0300

    Added 'D3' library and a sample bar graph

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,16 @@ request.open('GET', '/report.json', true)
 function onLoad() {
   if (this.status >= 200 && this.status < 400) {
     const data = JSON.parse(this.response)
-    console.log(data)
+    d3.select('#app')
+      .selectAll('div')
+      .data(data)
+      .enter()
+      .append('div')
+      .style('width', d => `${d.Msgs}px`)
+      .text((d) => {
+        const date = new Date(d.Data)
+        return `${d.Org} ${date.toLocaleDateString()}`
+      })
   } else {
     console.error(this.response)
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,10 @@ router.get('/css/bulma.min.css', (req, res) => {
   res.sendFile('bulma/css/bulma.css', { root })
 })
 
+router.get('/js/d3.js', (req, res) => {
+  res.sendFile('d3/build/d3.min.js', { root })
+})
+
 router.get('/report.json', data.json)
 
 router.get('/report.csv', data.csv)
