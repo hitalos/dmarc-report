fc7ba912627e4935d3b3219e4df4e705e4a14d33
Hítalo Silva <hitalos@gmail.com>
2017-10-19 19:10:02 -0300

    Added bulma framework

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,12 @@ const router = require('express').Router()
 
 const { data } = require('../controllers')
 
+const root = `${__dirname}/../../node_modules/`
+
+router.get('/css/bulma.min.css', (req, res) => {
+  res.sendFile('bulma/css/bulma.css', { root })
+})
+
 router.get('/report.json', data.json)
 
 router.get('/report.csv', data.csv)
