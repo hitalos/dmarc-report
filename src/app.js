952251c4c6bff87f9a79b3f5cec3b222453a5049
Hítalo Silva <hitalos@gmail.com>
2017-10-19 18:38:47 -0300

    Added error handlers

--- src/app.js
+++ src/app.js
@@ -10,4 +10,19 @@ app.use(express.static('public'))
 
 app.use(routes)
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  if (app.get('env') !== 'development') {
+    res.send(error.message)
+  } else {
+    res.send(error)
+  }
+})
+
 module.exports = app
