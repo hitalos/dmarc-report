3d873879ddf7cd0102651d647b5e7083beb28028
Hítalo Silva <hitalos@gmail.com>
2017-10-19 18:32:21 -0300

    Added helmet middleware for improve security

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const helmet = require('helmet')
 
 const routes = require('./routes')
 
 const app = express()
 
+app.use(helmet())
 app.use(express.static('public'))
 
 app.use(routes)
