9d5fbdeff44a63fd530330f5810287917f30017f
Hítalo Silva <hitalos@gmail.com>
2020-07-29 15:14:40 -0300

    Cleaned old bulma referencies (unused old dependency)

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,7 @@ const router = require('express').Router()
 
 const { data } = require('../controllers')
 
-const bulmaCSS = require.resolve('bulma/css/bulma.css')
 const highchartsJS = require.resolve('highcharts/highcharts.js')
-
-router.get('/css/bulma.min.css', (req, res) => {
-  res.sendFile(bulmaCSS)
-})
 
 router.get('/js/highcharts.js', (req, res) => {
   res.sendFile(highchartsJS)
